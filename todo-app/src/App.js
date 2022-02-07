8a24bb77703f2cf5467b050a641568c60d361aa8
Augustine Akoto Larbi-Ampofo <alarbiampofo@gmail.com>
2022-02-07 15:40:56 +0000

    redux dev tool fix

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -8,7 +8,7 @@ import rootReducer from "./store/reducers/rootReducer";
 //creating globalized state
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 function App() {
